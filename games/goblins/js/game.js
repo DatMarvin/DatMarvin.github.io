6c2c3a77e84bd1b3da5724a2e4fc87a8ceaa0a1f
Marvin Thelen <marvin.thelen@rwth-aachen.de>
2023-01-29 16:15:00 +0100

    '20.06.2020_16:33'

--- games/goblins/js/game.js
+++ games/goblins/js/game.js
@@ -128,3 +128,24 @@ requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame
 var then = Date.now();
 reset();
 main();
+
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var click = function () {
+	var client = new HttpClient();
+	client.get('https://google.com', function(response) {
+		console.log(response)
+	});
+}
---
+++ games/goblins/js/req.js
@@ -0,0 +1,19 @@
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+var click = function () {
+	var client = new HttpClient();
+	client.get('https://google.com', function(response) {
+		console.log(response)
+	});
+}
