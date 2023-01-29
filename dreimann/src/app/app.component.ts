f04f2e93ceb1cfcbd29866865aab7832f5cf3ebe
Marvin Thelen <marvin.thelen@rwth-aachen.de>
2023-01-29 17:15:50 +0100

    http init

--- dreimann/src/app/app.component.ts
+++ dreimann/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Dreimann';
   
+  const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
+  
+    constructor(private http:HttpClient) { 
+    //this.useHost = environment.host;//"localhost";//"87.187.44.125";
+   /* console.log("onini");
+    this.getIp().subscribe((result:any) => {
+      console.log("sub fertig");
+      if (environment.host === result.ip) {
+        this.useHost = "localhost";
+        console.log("usehost localhost");
+      }
+    });*/
+  }
+  
   onClickq() {
 	  console.log("ll")
+	    const url = `https://google.com`;
+    var body = "{}";//JSON.stringify();
+    return this.http.post<void>(url, body, httpOptions).subscribe(
+        res => {
+          console.log(res)
+    });
   }
 }
